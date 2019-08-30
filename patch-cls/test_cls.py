5b21266769f2e44e2e336981e505492be2b130c2
PingjunChen <chenpingjun@gmx.com>
2019-08-30 13:05:02 -0400

    update and validate test_cls

--- patch-cls/test_cls.py
+++ patch-cls/test_cls.py
@@ -6,30 +6,34 @@ import torch
 import torch.backends.cudnn as cudnn
 
 from train_eng import validate_model
-from data_ld import val_loader
+from patchloader import val_loader
+
 
 def set_args():
     parser = argparse.ArgumentParser(description='Thyroid Classification')
     parser.add_argument('--seed',            type=int,   default=1234)
+    parser.add_argument('--device_id',       type=str,   default="7",     help='which device')
     parser.add_argument('--model_dir',       type=str,   default="../data/Models/PatchModels")
     parser.add_argument('--data_name',       type=str,   default="thyroid")
-    parser.add_argument('--model_name',      type=str,   default="inceptionv3")
-    parser.add_argument('--model_path',      type=str,   default="thyroid11-0.9731.pth")
+    parser.add_argument('--model_name',      type=str,   default="resnet50")
+    parser.add_argument('--model_path',      type=str,   default="thyroid00-0.7621.pth")
+    parser.add_argument('--batch_size',      type=int,   default=32)
+    parser.add_argument('--patch_size',      type=int,   default=224)
 
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
-    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     args = set_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.device_id)
     torch.cuda.manual_seed(args.seed)
     cudnn.benchmark = True
 
-    model_full_path = os.path.join(args.model_dir, args.model_name, "MixBest", args.model_path)
+    model_full_path = os.path.join(args.model_dir, args.model_name, "01", args.model_path)
     ft_model = torch.load(model_full_path)
     ft_model.cuda()
-    test_loader = val_loader()
+    test_loader = val_loader(args.batch_size, args.patch_size)
 
     print("Start testing...")
     test_acc = validate_model(test_loader, ft_model, None)
